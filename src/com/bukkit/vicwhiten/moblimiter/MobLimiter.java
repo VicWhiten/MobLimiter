cfcc905adefd062102f3d30a0f5c557a9b185c86
Matt Rohr <mrohr@nearinfinity.com>
2011-03-25 15:52:58 -0400

    Added a /purgemobs command to kill all mobs

--- src/com/bukkit/vicwhiten/moblimiter/MobLimiter.java
+++ src/com/bukkit/vicwhiten/moblimiter/MobLimiter.java
@@ -1,15 +1,20 @@
 package com.bukkit.vicwhiten.moblimiter;
 
 import java.util.List;
+
+import org.anjocaido.groupmanager.GroupManager;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Creature;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityListener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
@@ -23,6 +28,7 @@ public class MobLimiter extends JavaPlugin
 	private final MobLimiterEntityListener entityListener = new MobLimiterEntityListener(this);
 	public int mobMax;
 	public Configuration config;
+	public GroupManager gm;
 
 	public void onDisable()
 	{
@@ -34,10 +40,20 @@ public class MobLimiter extends JavaPlugin
 	{
 
 		PluginManager pm = getServer().getPluginManager();
+		
+		 Plugin p = this.getServer().getPluginManager().getPlugin("GroupManager");
+	        if (p != null) {
+	            if (!this.getServer().getPluginManager().isPluginEnabled(p)) {
+	                this.getServer().getPluginManager().enablePlugin(p);
+	            }
+	            gm = (GroupManager) p;
+	        } 
+	 
 		config = this.getConfiguration();
 
 		pm.registerEvent(Event.Type.CREATURE_SPAWN, this.entityListener, Event.Priority.Normal, this);
 		getCommand("mobmax").setExecutor(new SetMaxCommand(this));
+		getCommand("purgemobs").setExecutor(new PurgeCommand(this));
 		
         PluginDescriptionFile pdfFile = this.getDescription();
         System.out.println(pdfFile.getName()+" version "+pdfFile.getVersion()+" is enabled!");
@@ -57,6 +73,20 @@ public class MobLimiter extends JavaPlugin
 		return mobs.size();
 	}
 	
+	public void purgeMobs(World world)
+	{
+		List<LivingEntity> mobs = world.getLivingEntities();
+		for(int j=0; j<mobs.size(); j++)
+		{
+			if(Creature.class.isInstance(mobs.get(j)))
+			{
+				LivingEntity mob = mobs.remove(j);
+				mob.remove();
+				j--;
+			}
+		}
+	}
+	
 	public void setupMobMax()
 	{
 		mobMax = config.getInt("mob-max", -1);
@@ -72,6 +102,17 @@ public class MobLimiter extends JavaPlugin
 	{
 		return mobMax;
 	}
+	
+    public boolean checkPermission(Player player, String permission)
+    {
+    	if(player.isOp())
+    	{
+    		return true;
+    	}else if(gm != null)
+    	{
+        return gm.getWorldsHolder().getWorldPermissions(player).has(player,permission);
+    	}else return false;
+    }
 	
 	private class MobLimiterEntityListener extends EntityListener {
 
@@ -95,13 +136,17 @@ public class MobLimiter extends JavaPlugin
 
 	    public SetMaxCommand(MobLimiter plugin) {
 	        this.plugin = plugin;
-	        System.out.println("Setting up /invademin Command");
 	    }
 
 	    public boolean onCommand(CommandSender sender, 
 	    		Command command, 
 	    		String label, String[] args) 
 	    {
+	    	if(!checkPermission((Player)sender, "moblimiter.setMax"))
+	    	{
+	    		sender.sendMessage(ChatColor.RED + "You do not have the permissions to do this");
+	    		return true;
+	    	}
 	    	if(args.length > 1)
 	    	{
 	    		return false;
@@ -123,5 +168,29 @@ public class MobLimiter extends JavaPlugin
 	    	return true;
 	    		
 	    }	
+	}
+	
+	private class PurgeCommand implements CommandExecutor 
+	{
+	    private final MobLimiter plugin;
+
+	    public PurgeCommand(MobLimiter plugin) {
+	        this.plugin = plugin;
+	    }
+
+	    public boolean onCommand(CommandSender sender, 
+	    		Command command, 
+	    		String label, String[] args) 
+	    {
+	    	if(!checkPermission((Player)sender, "moblimiter.purge"))
+	    	{
+	    		sender.sendMessage(ChatColor.RED + "You do not have the permissions to do this");
+	    		return true;
+	    	}
+	    	Player p = (Player)sender;
+	    	plugin.purgeMobs(p.getWorld());
+	    	return true;
+	    }
+	    
 	}
 }
